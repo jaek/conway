9d135be75a7123303f4c983e32a097850d6d75c8
Jake <jake.w.burns@gmail.com>
2018-06-19 17:51:43 +1000

    started on game logic

--- conway.js
+++ conway.js
@@ -4,24 +4,44 @@ window.onload = function () {
 
     var width = canvas.width;
     var height = canvas.height;
-    var px = 20;
-    var py = 20; //The height and width of pixels
+    let px = 20;
+    let py = 20; //The height and width of pixels
     var flag = true;
-    function draw_screen(ctx) {
+    function draw_screen(ctx, board) {
+        var xi = 0;
+        var yi = 0;
         for (x = 0; x < width; x += px) {
+            yi = 0;
             for (y = 0; y < height; y += py) {
-                if (flag === true) {
+                if (board[xi][yi] === 1) {
                     ctx.beginPath();
                     ctx.rect(x, y, px, py);
                     ctx.fill();
-                    flag = false;
-                } else {
-                    flag = true;
                 }
-
+                yi++;
             }
+            xi++;
         }
     }
+    function make_move(board, numrows, numcols) {
+        ;
+    }
+    //implementation of multidimensional array from The Good Parts 
+    Array.matrix = function (numrows, numcols, initial) {
+        var arr = [];
+        for (var i = 0; i < numrows; ++i) {
+            var columns = [];
+            for (var j = 0; j < numcols; ++j) {
+                columns[j] = initial;
+            }
+            arr[i] = columns;
+        }
+        return arr;
+    }
+    var test = Array.matrix(5, 5, 0);
+    test[0][0] = 1;
+    test[0][1] = 1;
+    draw_screen(context, test);
 
-    draw_screen(context);
+
 };
